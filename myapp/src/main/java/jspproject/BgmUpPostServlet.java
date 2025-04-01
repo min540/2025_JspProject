513bd2d6a8cb863cffd60ebe7e60dd89e24678c7
Jangton <dhwlsdn030@naver.com>
2025-04-01 18:56:07 +0900

    bgmmgr 추가 및 bgm, bgmup(업데이트)servlet 생성

---
+++ myapp/src/main/java/jspproject/BgmUpPostServlet.java
@@ -0,0 +1,25 @@
+package jspproject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/jspproject/bgmUpPost")
+public class BgmUpPostServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		BgmMgr bmgr = new BgmMgr();
+		if(bmgr.updateBgm(request)) {
+			response.sendRedirect("musiclist.jsp");
+		}else {
+			System.out.println("배경음악 수정 실패");
+			response.sendRedirect("musiclist.jsp?error=insertfail");
+		}
+	}
+
+}
