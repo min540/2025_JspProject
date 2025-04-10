a3986ba1be014da1b0e31fee2c754e4711115276
Yun_A <92855181+Dingrrrr@users.noreply.github.com>
2025-04-10 16:12:05 +0900

    통계 쪽 오류 나던 거 해결 완료

--- myapp/src/main/java/jspproject/ObjMgr.java
+++ myapp/src/main/java/jspproject/ObjMgr.java
@@ -237,39 +237,7 @@ public class ObjMgr {
 			pool.freeConnection(con, pstmt);
 		}
 	}
-	
-<<<<<<< HEAD
-	//작업 목록 리스트 받아오기
-	public Vector<ObjBean> getObjList(String user_id) {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql = null;
-		Vector<ObjBean> vlist = new Vector<ObjBean>();
-		try {
-			con = pool.getConnection();
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, user_id);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				ObjBean bean = new ObjBean();
-				bean.setObj_id(rs.getInt("obj_id"));
-				bean.setUser_id(rs.getString("user_id"));
-				bean.setObj_title(rs.getString("obj_title"));
-				bean.setObj_check(rs.getInt("obj_check"));
-				bean.setObj_regdate(SDF_DATE.format(rs.getDate("obj_regdate")));
-				bean.setObj_sdate(SDF_DATE.format(rs.getDate("obj_sdate")));
-				bean.setObj_edate(rs.getString("obj_edate"));
-				bean.setObjgroup_id(rs.getInt("objgroup_id"));
-				vlist.add(bean);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pool.freeConnection(con, pstmt, rs);
-		}
-		return vlist;
-=======
+
 	//작업 목록 리스트 받아오기(objgroup_id로 구분하도록 만듦), user_id
 	public Vector<ObjBean> getObjList(int objgroup_id, String user_id) {
 	    Connection con = null;
@@ -297,9 +265,9 @@ public class ObjMgr {
 	        pool.freeConnection(con, pstmt, rs);
 	    }
 	    return vlist;
->>>>>>> branch 'main' of https://github.com/HWAJINJJANG/2025_JspProject.git
 	}
-	//전체작업목표 받아오기
+	
+	// 전체 완료된 작업 목표 받아오기 (obj_check = 1인 것만)
 	public Vector<ObjBean> getTotalObjList(String user_id) {
 	    Connection con = null;
 	    PreparedStatement pstmt = null;
@@ -307,7 +275,8 @@ public class ObjMgr {
 	    Vector<ObjBean> vlist = new Vector<>();
 	    try {
 	        con = pool.getConnection();
-	        String sql = "SELECT obj_id, obj_title, obj_check, obj_edate FROM obj WHERE user_id=? ";
+	        String sql = "SELECT obj_id, obj_title, obj_check, obj_regdate, obj_edate " +
+	                     "FROM obj WHERE user_id = ? AND obj_check = 1";
 	        pstmt = con.prepareStatement(sql);      
 	        pstmt.setString(1, user_id);
 	        rs = pstmt.executeQuery();
@@ -316,7 +285,8 @@ public class ObjMgr {
 	            bean.setObj_id(rs.getInt("obj_id"));
 	            bean.setObj_title(rs.getString("obj_title"));
 	            bean.setObj_check(rs.getInt("obj_check"));
-	            bean.setObj_edate(rs.getString("obj_edate"));  // 형식 변환 필요 시 여기도 SDF_DATE 가능
+	            bean.setObj_regdate(rs.getString("obj_regdate"));
+	            bean.setObj_edate(rs.getString("obj_edate"));
 	            vlist.add(bean);
 	        }
 	    } catch (Exception e) {
@@ -326,7 +296,6 @@ public class ObjMgr {
 	    }
 	    return vlist;
 	}
-
 	
 	//작업 목록 카테고리 리스트 받아오기
 	public Vector<ObjGroupBean> getObjGroupList(String user_id) {
